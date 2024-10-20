d48ff06da451507e520c59bb127dfa2f3a54bb66
BGFF <76276738+aaa505958131@users.noreply.github.com>
2024-10-20 12:03:52 +0800

    Update TKFMtool.js

--- TKFMtool.js
+++ TKFMtool.js
@@ -1,6 +1,6 @@
 /*
 TKFMtool is a small tool used for the TKFM game.
-Copyright (C) BGFF  name of author
+Copyright (C) 2024  BGFF
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
